5ffc05da7d0fc02097d4b2243233d7fe57fe52da
Ulyana <68616130+khatkevichu@users.noreply.github.com>
2020-10-03 00:46:32 +0300

    fix: change all functions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ exports.min = function min (array) {
  for(let i = 1; i < array.length; i++){
    if(array[i] < min){
      min = array[i];   
-   } else if(array.lenght == 0){
-     min = 0;
+   } else {
+     return 0;
    }
   }
 return min;
@@ -16,8 +16,8 @@ exports.max = function max (array) {
  for(let i = 1; i < array.length; i++){
    if(array[i] > max){
      max = array[i];   
-   } else if(array.lenght == 0){
-     max = 0;
+   } else {
+     return 0;
    }
   }
 return max;
@@ -25,9 +25,9 @@ return max;
 
 exports.avg = function avg (array) {
   let sum = 0;
-  let count = array.length;
-  for (i =0; i<count; i++) {
+  let all = array.length;
+  for (i = 0; i < all; i++) {
     sum += array[i];
   }
-  return sum/count;
+  return sum/all;
 }
