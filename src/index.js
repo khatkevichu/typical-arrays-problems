c013bba9a114af39baa06e63cd987b69c2fffa69
Ulyana <68616130+khatkevichu@users.noreply.github.com>
2020-10-03 00:15:54 +0300

    init: start task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+ let min = array[0];
+ for(let i = 1; i < array.length; i++){
+   if(array[i] < min){
+     min = array[i];   
+   } else if(array.lenght == 0){
+     return 0;
+   }
+  }
+return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+ let max = array[0];
+ for(let i = 1; i < array.length; i++){
+   if(array[i] > max){
+     max = array[i];   
+   } else if(array.lenght == 0){
+     return 0;
+   }
+  }
+return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   return arr.reduce((a, b) => (a + b)) / arr.length;
 }
